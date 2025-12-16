3c4fd016a4590c619978cc50087b5062ab937a55
Amuguna <junjang99@naver.com>
2025-12-16 21:22:08 +0900

    feat: add kl-penalty scheduling option

--- module/train_sr_grpo.py
+++ module/train_sr_grpo.py
@@ -52,10 +52,7 @@ Respond in the following format:
 """
 
 
-def extract_xml_answer(text: str) -> str:
-    answer = text.split("<answer>")[-1]
-    answer = answer.split("</answer>")[0]
-    return answer.strip()
+
 
 
 def extract_hash_answer(text: str) -> str | None:
